7190428a1f98904cedc6e6f692e587a587afb039
Redlabot <112753718+Redlabot@users.noreply.github.com>
2022-09-13 01:58:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Denara Bot" //namabot kalian
-global.ownername= "DENARA" //nama kalian
+global.botname = "Elaina Chan" //namabot kalian
+global.ownername= "ReDLa" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/DenaraBot" //bebas asal jan hapus
-global.myweb2 = "https://linki.ee/denarabot" //bebas
-global.email = "denarabot1@gmail.com" //bebas
-global.region = "Indonesia" //bebas
+global.youtube = "https:/https://instagram.com/redlasenja?igshid=YmMyMTA2M2Y=" //bebas asal jan hapus
+global.myweb2 = "gada bree sumpah" //bebas
+global.email = "redlasans@gmail.com" //bebas
+global.region = "Mars" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6285727492435'] //premium user
-global.owner = ["6285727492435","6281215626270"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6285727492435" // nomor wa kalian
-global.ownernomerr = "+6285727492435" //nmr wa kalian
+global.premium = ['6281320511130'] //premium user
+global.owner = ["6281320511130","6285961503266"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6281320511130" // nomor wa kalian
+global.ownernomerr = "+6285961503266" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
-global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg." // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Denara Bot' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '® Redla Bot' //sticker wm ubah
+global.author = 'Di Buat Oleh ReDLa' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
